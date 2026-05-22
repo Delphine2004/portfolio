78428fe0c5ff167890483ef6a5fc0f5cb7434aef
Delphine FUMEX <dfumex2004@gmail.com>
2026-05-22 13:43:16 +0200

    Feat: ajout du filtre

--- public/assets/js/animation.js
+++ public/assets/js/animation.js
@@ -1,11 +1,25 @@
-const elements = document.querySelectorAll(".fade-in");
+import { applyFilters } from "./filter.js";
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("visible");
-    }
+document.addEventListener("DOMContentLoaded", () => {
+  // Récupération des éléments
+  const elements = document.querySelectorAll(".fade-in");
+  const filterInputs = document.querySelectorAll(".input-filter");
+  if (!filterInputs) return;
+
+  // Appliquer les effets
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      }
+    });
+  });
+
+  elements.forEach((el) => observer.observe(el));
+
+  // Filtrer en temps réel
+  filterInputs.forEach((input) => {
+    input.addEventListener("input", applyFilters);
+    input.addEventListener("change", applyFilters);
   });
 });
-
-elements.forEach((el) => observer.observe(el));
